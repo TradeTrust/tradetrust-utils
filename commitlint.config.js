9ee67f4c944fbae81f753042832bab0340daaa78
isaac <isaac.kps@gmail.com>
2021-12-08 15:06:59 +0800

    feat: update with initial config

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+
+  // Add your own rules. See http://marionebl.github.io/commitlint
+  rules: {
+    "body-max-line-length": [0], // disable
+  },
+};
