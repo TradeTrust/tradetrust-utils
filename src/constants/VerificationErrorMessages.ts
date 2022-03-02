0d688d8a17380a614c01e5ec77c4c40912742afd
isaackps <isaac.kps@gmail.com>
2022-03-02 16:30:49 +0800

    fix: lint

--- src/constants/VerificationErrorMessages.ts
+++ src/constants/VerificationErrorMessages.ts
@@ -49,7 +49,8 @@ export const MESSAGES = {
   [TYPES.NETWORK_INVALID]: {
     failureTitle: "Document's network field is invalid",
     successTitle: "",
-    failureMessage: "This document has an invalid network field. Please contact your issuing authority for help or issue the document before trying again.",
+    failureMessage: 
+      "This document has an invalid network field. Please contact your issuing authority for help or issue the document before trying again.",
   },
   [TYPES.CONTRACT_NOT_FOUND]: {
     failureTitle: "Document store or Token registry address cannot be found",
