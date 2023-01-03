f33b45a0d1c5ffec8e1854da56ac1b23e4caaf4d
Sim Boon Long <simboonlong@gmail.com>
2023-01-03 13:51:07 +0800

    fix: chain urls

--- src/constants/supportedChains.ts
+++ src/constants/supportedChains.ts
@@ -10,49 +10,91 @@ export enum CHAIN_ID {
 }
 
 export type chainInfo = {
+  id: CHAIN_ID;
   label: string;
   name: networkName;
   type: networkType;
   currency: networkCurrency;
+  explorerUrl: string;
+  explorerApiUrl?: string;
+  rpcUrl?: string;
+  nativeCurrency?: {
+    name: string;
+    symbol: string;
+    decimals: number;
+  };
 };
 
 type supportedChains = Record<CHAIN_ID, chainInfo>;
 
 export const SUPPORTED_CHAINS: supportedChains = {
   [CHAIN_ID.local]: {
+    id: CHAIN_ID.local,
     label: "Local",
     name: "local",
     type: "development",
     currency: "ETH",
+    explorerUrl: "https://localhost/explorer",
   },
   [CHAIN_ID.mainnet]: {
+    id: CHAIN_ID.mainnet,
     label: "Mainnet",
     name: "mainnet",
     type: "production",
     currency: "ETH",
+    explorerUrl: "https://etherscan.io",
+    explorerApiUrl: "https://api.etherscan.io",
   },
   [CHAIN_ID.matic]: {
+    id: CHAIN_ID.matic,
     label: "Polygon",
     name: "matic",
     type: "production",
     currency: "MATIC",
+    explorerUrl: "https://polygonscan.com",
+    rpcUrl: "https://polygon-rpc.com",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
   },
   [CHAIN_ID.maticmum]: {
+    id: CHAIN_ID.maticmum,
     label: "Polygon Mumbai",
     name: "maticmum",
     type: "test",
     currency: "MATIC",
+    explorerUrl: "https://mumbai.polygonscan.com",
+    explorerApiUrl: "https://api-testnet.polygonscan.com",
+    rpcUrl: "https://rpc-mumbai.matic.today",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "mMATIC",
+      decimals: 18,
+    },
   },
   [CHAIN_ID.goerli]: {
+    id: CHAIN_ID.goerli,
     label: "Goerli",
     name: "goerli",
     type: "test",
     currency: "ETH",
+    explorerUrl: "https://goerli.etherscan.io",
+    explorerApiUrl: "https://api-goerli.etherscan.io",
   },
   [CHAIN_ID.sepolia]: {
+    id: CHAIN_ID.sepolia,
     label: "Sepolia",
     name: "sepolia",
     type: "test",
     currency: "ETH",
+    explorerUrl: "https://sepolia.etherscan.io",
+    rpcUrl: "https://rpc.sepolia.org",
+    nativeCurrency: {
+      name: "ETH",
+      symbol: "sepETH",
+      decimals: 18,
+    },
   },
 };
