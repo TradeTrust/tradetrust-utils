f6486d9bfb969c9638e9b1daf76fea28bea01651
Sim Boon Long <simboonlong@gmail.com>
2023-01-10 10:03:12 +0800

    fix: networks

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -1,4 +1,6 @@
-export type networkName = "local" | "mainnet" | "matic" | "maticmum" | "goerli" | "sepolia";
+export const networks = ["local", "mainnet", "matic", "maticmum", "goerli", "sepolia"] as const;
+
+export type networkName = typeof networks[number];
 
 export type networkType = "production" | "test" | "development";
 
