1efaacfeab3d69d3ac095aa0056c4240e4442fb7
isaackps <isaac.kps@gmail.com>
2023-06-09 13:01:03 +0800

    Fix: update dependencies and format with the new dependencies

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -1,6 +1,6 @@
 export const networks = ["local", "mainnet", "matic", "maticmum", "goerli", "sepolia"] as const;
 
-export type networkName = typeof networks[number];
+export type networkName = (typeof networks)[number];
 
 export type networkType = "production" | "test" | "development";
 
