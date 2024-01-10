1bae04ec3bab84fe7b467300abae1d4da4caeeba
Daniel Blanco Parla <dblancoparla@gmail.com>
2024-01-10 16:25:04 +0100

    feat(network): Add stabilitytestnet network and FREE currency

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -1,17 +1,7 @@
-export const networks = [
-  "local",
-  "mainnet",
-  "matic",
-  "maticmum",
-  "sepolia",
-  "xdc",
-  "xdcapothem",
-  "hederatestnet",
-  "hederamainnet",
-] as const;
+export const networks = ["local", "mainnet", "matic", "maticmum", "sepolia", "xdc", "xdcapothem", "hederatestnet", "hederamainnet", "stabilitytestnet"] as const;
 
 export type networkName = (typeof networks)[number];
 
 export type networkType = "production" | "test" | "development";
 
-export type networkCurrency = "ETH" | "MATIC" | "XDC" | "HBAR";
+export type networkCurrency = "ETH" | "MATIC" | "XDC" | "HBAR" | "FREE";
