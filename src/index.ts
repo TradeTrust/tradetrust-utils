cc63ae968129798141d941a866ce8effd0cff6c0
Ng Han Inn <43451336+nghaninn@users.noreply.github.com>
2025-02-04 10:28:37 +0800

    fix: copy gas station into utils (#61)
    * fix: copy gas station into utils
    
    * chore: update package version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as CONSTANTS from "./constants/VerificationErrorMessages";
 export * from "./constants/supportedChains";
+export * from "./constants/network";
 export * from "./analytics";
 export * from "./fragments";
+export * from "./gasStation";
 export { CONSTANTS };
